b51d198261eb41206c6ca36a5d1e40f12175f05d
Matt Arcidy <marcidy@gmail.com>
2020-01-04 22:04:37 -0800

    Doc strings

--- main.py
+++ main.py
@@ -8,20 +8,23 @@ from util import (
 
 
 DEBUG = True
-ACTIVITY_TIMEOUT = 5*60  # 5min activity timeout
+ACTIVITY_TIMEOUT = 10*60  # 10min activity timeout
 LASER_COST = 0.5
 
 
 class StateValues(Enum):
+    ''' Enum for controller states '''
     INIT = 0
-    AUTHORIZED = 1
-    ENABLED = 2
-    FIRING = 3
-    DEAUTHORIZED = 4
-    DISABLED = 5
+    ENABLED = 1
+    FIRING = 2
 
 
 class Timer:
+    '''Basic timer which starts, stops, reports if it's runnings, etc.
+
+    :param seconds: Length between timer.start called and timer.check is True
+    :type seconds: float
+    '''
 
     def __init__(self, seconds):
         self.seconds = seconds
@@ -30,32 +33,40 @@ class Timer:
         self.start_time = 0
 
     def start(self):
+        ''' sets self.running to True and sets self.start_time '''
         self.running = True
         self.start_time = time.time()
 
     def check(self):
-        '''True if timer is disabled or not timed out'''
+        '''True if timer is disabled or not timed out
+        sets self.timeout to True if timer is expired
+
+        :returns: if timer is still active
+        :rtype: bool
+        '''
         self.timeout = (not self.running or
                         time.time() - self.start_time < self.seconds)
         return self.timeout
 
     def stop(self):
+        '''Stops the timer, sets self.running to False'''
         self.running = False
 
     def reset(self):
+        '''Resets the start time, resetting the timer without stopping it'''
         self.start_time = time.time()
 
 
 class Laser:
     '''The pi interfaces with the laser through a Teensy connected via
     a USB port.  The Teensy responds to binary encoded ascii characters,
-    and sends binary encoded ascii strings.  The complete interface is
-    documented :FIXME:.
+    and sends binary encoded ascii strings.  The interface is
+    documented here: https://github.com/acemonstertoys/laser-rfid
 
     The USB port needs to be reset on initialization to capture it.  This
     requires writing to the USB_PATH as root.
 
-    The Teensy itself runs firmware from :FIXME:
+    The Teensy itself runs firmware from the same repo as above.
 
     The Teensy responds to a status command 'o' with the current time
     it's been firing and a flag indiciating a new rfid scan.
@@ -71,8 +82,13 @@ class Laser:
     rfid token is authorized.)
 
     The laser is a reporting a simple state when polled.  So the pi must
-    continually poll the laser state and manage it's authorization state.'''
+    continually poll the laser state and manage it's authorization state.
 
+    :param port: system serial port path
+    :type port: string
+    :param baudrate: serial port baudrate
+    :type baudrate: int
+    '''
     USB_PATH = "/sys/bus/usb/devices/usb1/authorized"
     # would prefer if the Teensy weren't so opinionated about what to do.
     # why save to eeprom when you can just send information about if the
@@ -97,6 +113,10 @@ class Laser:
         return self.conn.read_until(b'\n')
 
     def read(self):
+        ''' Grabs data from the serial port, decodes, and strips protocol
+        control chars, currently '\n'.  This function should be used rather
+        than reading hte Sesrial connection directly for uniformity.
+        '''
         more = self.conn.in_waiting > 0
         data = b''
         while more:
@@ -109,20 +129,24 @@ class Laser:
             return ''
 
     def enable(self):
+        ''' Enables the laser through Teensy '''
         self.write("e")
         self.enabled = True
 
     def disable(self):
+        ''' Disable the laser through Teensy '''
         self.write("d")
         self.enabled = False
 
     def reset_usb(self):
+        ''' Resets the USB port, specific to sysfs paths'''
         with open(self.USB_PATH, 'w') as f:
             f.write('0')
             time.sleep(1)
             f.write('1')
 
     def display(self, line1='', line2=''):
+        ''' Displays on the 2-line VCD display connected to Teesy '''
         if line1:
             self.write('p'+line1)
         if line2:
@@ -144,6 +168,9 @@ class Laser:
                 print("Error: status - {}".format(data))
 
     def rfid(self):
+        ''' requests rfid string from Teensy, returns 8 bits only.  For
+        compatibility, can use 8bit or 10bits.
+        '''
         self.write("r")
         time.sleep(.1)
         data = self.read()[1:]
@@ -153,12 +180,15 @@ class Laser:
             return data[2:]
 
     def reset_cut_time(self):
+        ''' Part of Teensy laser interface.  Resets odometer on teensy'''
         self.write('x')
 
     def update_cut_time(self):
+        ''' Explicit update of odometer'''
         self.write('y')
 
     def read_cut_time(self):
+        ''' Retrieve odometer from Teensy'''
         self.write('z')
 
     def cost(self, firing_time):
@@ -166,6 +196,9 @@ class Laser:
 
 
 class AuthManager:
+    ''' Abstracts authentication state.  Warehouses rfid updating,
+    authorization, and authorized id.
+    '''
 
     def __init__(self):
         print("Laser service starting....")
@@ -176,19 +209,35 @@ class AuthManager:
         self.authorized = False
 
     def update_rfids(self):
+        ''' sets whitelist.  Should be called on a timer in the main loop '''
         self.whitelist = load_whitelist()
 
     def check_rfid(self, rfid):
+        ''' Compares rfid to whitelist, return True if rfid in white list,
+        False if not
+        :param rfid: Hex string of rfid to check
+        :type rfid: string
+
+        :returns: True if rfid is in whitelist, false if not
+        :rtype: bool
+        '''
         report_attempt(rfid, self.authorized)
         return rfid in self.whitelist
 
     def login(self, rfid):
+        ''' logs a user in by rfid if rfid is valid, logs to system out
+
+        :param rfid: Hex string of rfid to check
+        :type rfid: string
+        '''
         if self.check_rfid(rfid):
             self.authorized = True
             self.authorized_rfid = rfid
             print("Logged in: {}".format(rfid))
 
     def logout(self):
+        ''' logs user out'''
+
         if self.authorized:
             print("Logging out: {}".format(self.authorized_rfid))
         self.authorized = False
@@ -196,6 +245,26 @@ class AuthManager:
 
 
 class Controller:
+    ''' The controller takes instanciated AuthManager and resource (e.g., the
+    laser) and computes the current state, state transition, and manages the
+    state transition.  It's a statemachine.  Hardware is a statemachine, and
+    state machines are highly tractible to analyze for completeness, thus are
+    ideal for cointrolling hardware.
+
+    The below implementation can be generalized to other resources fairly
+    easily, but not all resources behave like the laser.  Currently the laser
+    has display, rfid scanning, and firing time all on the same interface,
+    so a more general approach would abstract these inputs further.
+
+    The only thing the main loop interacts with is the controller, which uses
+    the AuthManager and inspects the resource.
+
+    :param manager: AuthManager instance
+    :type manager: AuthManager
+    :param resource: Currently Laser is an example of a compliant resource
+    :type resource: something which implements an odometer, rfid scanning,
+    enable/disabling, and cost.
+    '''
 
     def __init__(self, manager, resource):
         self.manager = manager
@@ -207,6 +276,9 @@ class Controller:
         self.firing_end = 0
 
     def scan(self):
+        ''' retrieves an rfid from the resource and decides what to do with it
+        based on the state of the auth manager'''
+
         current_rfid = self.manager.authorized_rfid
         new_rfid = self.resource.rfid()
         self.resource.rfid_flag = '0'
@@ -218,6 +290,7 @@ class Controller:
             self.manager.login(new_rfid)
 
     def emit_state(self):
+        ''' Retrieve state variables from resource and manager '''
         return {
             'odometer': self.resource.odometer,
             'enabled': self.resource.enabled,
@@ -225,15 +298,21 @@ class Controller:
             'scanned': self.resource.rfid_flag == '1'}
 
     def calculate_state(self):
+        ''' From current state information, the next state if calculated.  This
+        function explicitly separates the concerns of computing the next state
+        from transitioning to the next state.  This makes transitions explicit
+        and separates out the dependencies.
+        '''
         new_internal_state = self.emit_state()
         scanned = new_internal_state['scanned']
 
-        if self.state == StateValues.INIT and scanned:
-            # only scans new fobs when in INIT state
+        # only scans new fobs when in INIT state
+        if scanned and self.state in [StateValues.INIT, StateValues.ENABLED]:
             self.scan()
             # overwrite new state taking into account scan result
             new_internal_state = self.emit_state()
 
+        # state variables
         enabled = new_internal_state['enabled']
         laser_on = (new_internal_state['odometer'] >
                     self.internal_states['odometer'])
@@ -264,9 +343,27 @@ class Controller:
         return next_state
 
     def run(self):
+        ''' Run is the only function to call, the next state is calculated
+        by calling calculate_state, and hte body of the function manages the
+        state transition.
+
+        the state machines is designed to explicitly depend only on this state
+        and the known next state.  This allows the state machines to be
+        easy to analyze for illegal transitions, and complete coverage
+        of the logic to transition between states.
+
+        The state machiens is written as a logic statement to describe the
+        transition, and what to do to perform the transition.
+
+        The transitions are not mutually exclusive
+
+        '''
         self.resource.status()
         next_state = self.calculate_state()
 
+        # The initial state means teh resource is disabled and the devices is
+        # not authorized.
+        # The activity timer is also disabled.
         if next_state == StateValues.INIT:
             self.resource.disable()
             self.manager.logout()
@@ -274,13 +371,19 @@ class Controller:
             self.activity_timer.reset()
             self.resource.display("Please swipe", "fob to continue")
 
+        # If we are transitioning from the INIT state, beging the activity
+        # timer.
         if self.state == StateValues.INIT and next_state != StateValues.INIT:
             self.activity_timer.start()
 
+        # If we are moving from INIT to ENABLED, enable the resource during
+        # the transition
         if self.state == StateValues.INIT and next_state == StateValues.ENABLED:
             self.resource.enable()
             self.resource.display("Welcome", self.manager.authorized_rfid)
 
+        # If we are transitioning to FIRING from not firing, update the
+        # activity timer, and begin tracking firing time
         if self.state != StateValues.FIRING and next_state == StateValues.FIRING:
             # set up cut and start tracking time and cost
             self.activity_timer.reset()
@@ -288,12 +391,14 @@ class Controller:
             self.resource.display("Elapsed: 1", "Cost: 0")
             print("Firing Start: {}".format(self.firing_start))
 
+        # if we are currently firing and we're still firing, update the timer
         if self.state == next_state and self.state == StateValues.FIRING:
             # update screen with time / cost
             self.activity_timer.reset()
             current_time = min(1, time.time() - self.firing_start)  # in seconds
             self.display(current_time)
 
+        # If we are done firing, stop the timer, and report the cut
         if self.state == StateValues.FIRING and next_state != StateValues.FIRING:
             # end cut, report cost, etc,
             end_time = time.time()
@@ -310,9 +415,16 @@ class Controller:
         if DEBUG and self.state != next_state:
             print("{} --> {}".format(self.state, next_state))
 
+        # Transition the state variable
         self.state = next_state
 
     def display(self, firing_time):
+        ''' Helper function to display the time and cost of an ongong cut
+
+        :param firing_time: Time, in seconds
+        :type firing_time: int
+        '''
+
         display_cost = round(self.resource.cost(firing_time), 2)
         mins = int(firing_time / 60)
         secs = firing_time % 60
